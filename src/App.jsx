d38e2b44fea284f0f5cca854dc1cdbe6137ddf1a
parth-vaghela005 <vaghelaparth300@gmail.com>
2025-04-16 16:45:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <button className='border-2 border-l-black px-5 py-5 bg-gray-500 hover:bg-gray-700 text-white font-bold rounded-lg'>
+        Click
+      </button>
+    </>
+  )
+}
+
+export default App
