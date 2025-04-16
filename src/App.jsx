cf561307777c9d0baacd19360da0b7ce58b2ad6e
parth-vaghela005 <vaghelaparth300@gmail.com>
2025-04-16 16:46:30 +0530

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import './App.css'
 function App() {
   return (
     <>
-      <button className='border-2 border-l-black px-5 py-5 bg-gray-500 hover:bg-gray-700 text-white font-bold rounded-lg'>
-        Click
-      </button>
+     <h1>
+      Hello, World!
+     </h1>
     </>
   )
 }
